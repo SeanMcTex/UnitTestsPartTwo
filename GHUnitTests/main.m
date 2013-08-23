14de539af320e187697dc80959b4d5b340c7456d
Sean McMains <sean@mcmains.net>
2013-08-23 09:50:27 -0500

    Project created, GHUnit in place.

---
+++ GHUnitTests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GHUnitTests
+//
+//  Created by Sean McMains on 8/23/13.
+//  Copyright (c) 2013 Mutual Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate" );
+    }
+}
